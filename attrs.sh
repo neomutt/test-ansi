f1202717c114789ba73d5f13bc605e6025765aee
Richard Russon <rich@flatcap.org>
2018-09-29 13:40:37 +0100

    add blink

--- attrs.sh
+++ attrs.sh
@@ -4,11 +4,12 @@
 # 2 Faint (decreased intensity) Not widely supported.
 # 3 Italic Not widely supported. Sometimes treated as inverse.
 # 4 Underline 
+# 5 Blink
 # 7 reverse video swap foreground and background colors
 # 8 Conceal Not widely supported.
 # 9 Crossed-out Characters legible, but marked for deletion. Not widely supported.
 
-for i in 1 2 3 4 7 8 9; do
-	echo -e "$i \e[${i}mABCDEFGHIJKLMNOPQRSTUVWXYZ\e[0m"
+for i in 1 2 3 4 5 7 8 9; do
+	echo -e "$i \\e[${i}mABCDEFGHIJKLMNOPQRSTUVWXYZ\\e[0m"
 done
 
