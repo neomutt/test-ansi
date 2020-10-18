8d10fa3334f7a55d6ce4d6edbc3ad1d630284365
Richard Russon <rich@flatcap.org>
2020-10-18 16:25:33 +0100

    reorg

--- date-top-right.sh
+++ date-top-right.sh
@@ -3,7 +3,9 @@
 # save cursor position, move to row;col, date, restore cursor
 
 DATE="$(date +%F)"
-COLUMN=$(tput cols)
-COLUMN=$((COLUMN - ${#DATE} + 1))
+COLUMN=$(($(tput cols) - ${#DATE} + 1))
+
+echo -n "before ->"
 echo -en "\e[s\e[1;${COLUMN}H${DATE}\e[u"
+echo "<- after"
 
