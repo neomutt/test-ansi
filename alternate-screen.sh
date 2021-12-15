4202de7be844b5f9e2ec4b2e6d64b791ea338c82
Richard Russon <rich@flatcap.org>
2021-12-15 13:50:32 +0000

    colour/attr alternate screen

--- alternate-screen.sh
+++ alternate-screen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Normal screen"
+echo -e "\e[1;31mNormal screen\e[0m"
 sleep 1
 echo -en "\e[?1049h"
-echo Alternate screen
+echo -e "\e[1;4;32mAlternate screen\e[0m"
 sleep 1
 echo -en "\e[?1049l"
-echo "Normal screen again"
+echo -e "\e[1;34mNormal screen again\e[0m"
 
