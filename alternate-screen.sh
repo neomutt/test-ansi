d988ee94cb2758fb96582fb1689b181586ee7ca3
Richard Russon <rich@flatcap.org>
2022-02-07 11:27:49 +0000

    alternate-screen.sh: add wrapped url

--- alternate-screen.sh
+++ alternate-screen.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+URL="http://example.com/?q=retinued+pinocle+analabos+turnhall+spald+dozener+lustred+bangtail+ghawazi+granddad+phasma+santir+deplanes+caduke+wimples+occur+rankish+models+pandle+scaldino+rickets+maddened+majolica+irisated+massif+reweighs+embers+phloems+femmes+babblers+propine+priscan+bisters+royale+clouding+huguenot+slampant+rulable+tropaia+harder+retied+ponica+tabbies+moodier+ciniphes+gustier+overwhip+murre+cesare+saggier"
+
 echo -e "\e[1;31mNormal screen\e[0m"
-sleep 1
+echo "$URL"
+sleep 3
 echo -en "\e[?1049h"
 echo -e "\e[1;4;32mAlternate screen\e[0m"
-sleep 1
+echo "$URL"
+sleep 3
 echo -en "\e[?1049l"
 echo -e "\e[1;34mNormal screen again\e[0m"
+echo "$URL"
 
